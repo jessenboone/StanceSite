6dc2316ad13dabebca19ab16984af75cb120f52d
Your Name <you@example.com>
2017-05-20 14:03:39 -0700

    added gulp, .gitignore, packege.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,26 @@
-var gulp = require('gulp')
-, sourcemaps = require('gulp-sourcemaps')
-, sass = require('gulp-sass')
-, concat = require('gulp-concat')
-, CacheBuster = require('gulp-cachebust')
-, print = require('gulp-print')
-, babel = require('gulp-babel');
-// , uglify = require('gulp-uglify');
+var gulp = require("gulp");
+var sourcemaps = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+var CacheBuster = require("gulp-cachebust");
+var print = require("gulp-print");
+var babel = require("gulp-babel");
+var uglify = require("gulp-uglify");
 
-var cachebust = new CacheBuster();
+var cachebust = new CacheBuster;
 
-gulp.task('build-css', function() {
-  return gulp.src(['./styles/*.scss', './styles/*.css'])
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(cachebust.resources())
-    .pipe(concat('styles.css'))
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./dist'));
-})
-
-gulp.task('build-images', function() {
-  return gulp.src('./images/**/*')
-    .pipe(gulp.dest('./dist/images'))
-})
-
-gulp.task('build-views', function() {
-  return gulp.src('./views/*.html')
-    .pipe(gulp.dest('./dist/views'))
+gulp.task("build-css", function(){
+  return gulp.src("./frontend/*.scss")
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(cachebust.resources())
+  .pipe(concat("styles.css"))
+  .pipe(sourcemaps.write("./maps"))
+  .pipe(gulp.dest("./dist"));
 })
 
 gulp.task('build-js', function() {
-   return gulp.src(['js/app.js','js/**/*.js'])
+   return gulp.src('frontend/**/*.js')
       .pipe(sourcemaps.init())
       .pipe(print())
       .pipe(babel({ presets: ['es2015'] }))
@@ -40,12 +30,12 @@ gulp.task('build-js', function() {
       .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('build', ['build-css', 'build-js', 'build-images', 'build-views'], function() {
+gulp.task('build', ['build-css', 'build-js'], function() {
     return gulp.src('index.html')
         .pipe(cachebust.references())
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
-    return gulp.watch(['./index.html', './styles/*.*css', './js/**/*.js', './views/*.html', './images/**/*'], ['build']);
+    return gulp.watch(['./index.html','./partials/*.html', './styles/*.*css', './js/**/*.js'], ['build']);
 });
