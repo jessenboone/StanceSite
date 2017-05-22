4c929cc3e97728b79e3cde79c7b3f0efa66a1cfc
codingplumber <jason.stickel@live.com>
2017-05-22 12:21:01 -0600

    added functions in frontend controllers and mainSrvc

--- frontend/js/mensCtrl.js
+++ frontend/js/mensCtrl.js
@@ -4,7 +4,17 @@ angular.module('app')
   $scope.test = 'mens working';
   $scope.test2 = mainSrvc.test;
 
+  $scope.getProducts = () => {
+    mainSrvc.getProducts().then(function(response) {
+      $scope.products = response;
+    });
+  };
+  getProducts();
 
-
+  $scope.getProductsByCategory = (mens) => {
+    mainSrvc.getProductsByCategory(mens).then(function(response) {
+      $scope.mensProducts = response;
+    });
+  };
 
 });
