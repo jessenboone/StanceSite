07602420c7da285a9a82366f670ffec118e10a41
codingplumber <jason.stickel@live.com>
2017-05-26 09:08:49 -0600

    update

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -383,12 +383,23 @@ angular.module('app').controller('singleProductCtrl', function ($scope, mainSrvc
   $scope.test = 'single product working';
   $scope.test2 = mainSrvc.test;
 
+  $scope.div1 = true;
+  $scope.div2 = true;
+  $scope.div3 = true;
+
   $scope.getSingleProduct = function () {
     mainSrvc.getSingleProduct($stateParams.product_id).then(function (response) {
       $scope.singleProduct = response;
     });
   };
   $scope.getSingleProduct();
+
+  $scope.showHide = function (pic) {
+    $scope.pic1 = false;
+    $scope.pic2 = false;
+    $scope.pic3 = false;
+    $scope.pic = true;
+  };
 });
 'use strict';
 
--- frontend/js/singleProductsCtrl.js
+++ frontend/js/singleProductsCtrl.js
@@ -4,6 +4,10 @@ angular.module('app')
   $scope.test = 'single product working';
   $scope.test2 = mainSrvc.test;
 
+  $scope.div1 = true;
+  $scope.div2 = true;
+  $scope.div3 = true;
+
   $scope.getSingleProduct = () => {
     mainSrvc.getSingleProduct($stateParams.product_id).then(function(response) {
       $scope.singleProduct = response;
@@ -11,6 +15,11 @@ angular.module('app')
   }
   $scope.getSingleProduct();
 
-
+  $scope.showHide = (pic) => {
+    $scope.pic1 = false;
+    $scope.pic2 = false;
+    $scope.pic3 = false;
+    $scope.pic = true;
+  }
 
 });
