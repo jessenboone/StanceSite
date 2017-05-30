e24e99c94155fa6d4871c08181dc7664445b80cc
codingplumber <jason.stickel@live.com>
2017-05-30 17:00:44 -0600

    update

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -425,17 +425,48 @@ angular.module('app').directive('randomDirective', function (mainSrvc) {
 
 angular.module('app').controller('registerCtrl', function ($scope, mainSrvc) {
 
-  $scope.test = 'register working';
-  $scope.test2 = mainSrvc.test;
-
   $scope.isShown = true;
   $scope.isShown2 = true;
 
-  $scope.register = function () {
-    console.log('button working!');
-    mainSrvc.register($scope.user).then(function (response) {
-      /*may need to set default for newsletter*/
-    });
+  $scope.emailVeri = false;
+
+  $scope.register = function (user) {
+    var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+    if (user.email !== user.email_confirm) {
+      $scope.match2 = true;
+      return;
+    }
+    console.log(EMAIL_REGEXP.test(user.email));
+    if (!EMAIL_REGEXP.test(user.email)) {
+      $scope.match = true;
+      return;
+    } else if (user.password.length < 8) {
+      $scope.match3 = true;
+      return;
+    } else if (user.password !== user.password_confirm) {
+      $scope.match4 = true;
+      return;
+    } else {
+      mainSrvc.register(user).then(function (response) {
+        user.first_name = '';
+        user.last_name = '';
+        user.email = '';
+        user.email_confirm = '';
+        user.password = '';
+        user.password_confirm = '';
+        $scope.firstNameEmpty = false;
+        $scope.lastNameEmpty = false;
+        $scope.emailEmpty = false;
+        $scope.emailConfirmEmpty = false;
+        $scope.passwordEmpty = false;
+        $scope.passwordConfirmEmpty = false;
+        $scope.match = false;
+        $scope.match2 = false;
+        $scope.match3 = false;
+        $scope.match4 = false;
+        /*may need to set default for newsletter*/
+      });
+    }
   };
 });
 'use strict';
